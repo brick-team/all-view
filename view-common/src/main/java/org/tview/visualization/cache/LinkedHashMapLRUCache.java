eddf3677948ae16d7a7cd9b97d0dc6a2959a5545
huifer <huifer97@163.com>
2020-07-07 22:01:31 +0800

    feature/cache

--- view-common/src/main/java/org/tview/visualization/cache/LinkedHashMapLRUCache.java
+++ view-common/src/main/java/org/tview/visualization/cache/LinkedHashMapLRUCache.java
@@ -4,39 +4,37 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Set;
 
-/**
- * LRU 最少使用的会被删除
- */
+/** LRU 最少使用的会被删除 */
 public class LinkedHashMapLRUCache {
 
-    private final int size;
-    LinkedHashMap<String, String> cache;
+  private final int size;
+  LinkedHashMap<String, String> cache;
 
-    public LinkedHashMapLRUCache(int size) {
-        this.cache = new LinkedHashMap<>(size);
-        this.size = size;
+  public LinkedHashMapLRUCache(int size) {
+    this.cache = new LinkedHashMap<>(size);
+    this.size = size;
+  }
+
+  public Object get(String key) {
+
+    if (!cache.containsKey(key)) {
+      return null;
     }
+    String value = cache.get(key);
+    cache.remove(key);
+    cache.put(key, value);
+    return value;
+  }
 
-    public Object get(String key) {
+  public void put(String key, String value) {
+    cache.remove(key);
 
-        if (!cache.containsKey(key)) {
-            return null;
-        }
-        String value = cache.get(key);
-        cache.remove(key);
-        cache.put(key, value);
-        return value;
+    if (size == cache.size()) {
+      // remove first element
+      Set<String> keySet = cache.keySet();
+      Iterator<String> iterator = keySet.iterator();
+      cache.remove(iterator.next());
     }
-
-    public void put(String key, String value) {
-        cache.remove(key);
-
-        if (size == cache.size()) {
-            // remove first element
-            Set<String> keySet = cache.keySet();
-            Iterator<String> iterator = keySet.iterator();
-            cache.remove(iterator.next());
-        }
-        cache.put(key, value);
-    }
+    cache.put(key, value);
+  }
 }
--- view-common/src/main/java/org/tview/visualization/cache/MapLRUCache.java
+++ view-common/src/main/java/org/tview/visualization/cache/MapLRUCache.java
@@ -6,24 +6,25 @@ import java.util.Map.Entry;
 
 public class MapLRUCache {
 
-    Map<String, String> map;
-    int size;
+  Map<String, String> map;
+  int size;
 
-    public MapLRUCache(int size) {
-        this.size = size;
-        map = new LinkedHashMap<>() {
-            @Override
-            protected boolean removeEldestEntry(Entry<String, String> entry) {
-                return size() > size;
-            }
+  public MapLRUCache(int size) {
+    this.size = size;
+    map =
+        new LinkedHashMap<>() {
+          @Override
+          protected boolean removeEldestEntry(Entry<String, String> entry) {
+            return size() > size;
+          }
         };
-    }
+  }
 
-    public String get(String key) {
-        return map.get(key);
-    }
+  public String get(String key) {
+    return map.get(key);
+  }
 
-    public void put(String k, String v) {
-        map.put(k, v);
-    }
+  public void put(String k, String v) {
+    map.put(k, v);
+  }
 }
