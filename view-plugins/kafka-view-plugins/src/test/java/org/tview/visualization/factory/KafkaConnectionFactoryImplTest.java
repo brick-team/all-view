cbdabb5355a3b5bad58d6dfd19697a214b201610
huifer <huifer97@163.com>
2020-07-20 20:12:57 +0800

    fix: dependency

--- view-plugins/kafka-view-plugins/src/test/java/org/tview/visualization/factory/KafkaConnectionFactoryImplTest.java
+++ view-plugins/kafka-view-plugins/src/test/java/org/tview/visualization/factory/KafkaConnectionFactoryImplTest.java
@@ -1,12 +1,14 @@
 package org.tview.visualization.factory;
 
 import org.apache.kafka.clients.admin.AdminClient;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class KafkaConnectionFactoryImplTest extends AbsKafkaConfig {
 
   KafkaConnectionFactory factory = new KafkaConnectionFactoryImpl();
 
+  @DisplayName("org.tview.visualization.factory.KafkaConnectionFactoryImpl.factoryAdminClient")
   @Test
   void factoryAdminClient() {
     AdminClient adminClient = factory.factoryAdminClient(this.config());
