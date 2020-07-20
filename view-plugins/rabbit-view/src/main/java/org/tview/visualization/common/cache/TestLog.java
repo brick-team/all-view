525adfcfe59139703c06d504cef63f29f8e750a1
huifer <huifer97@163.com>
2020-07-20 19:53:50 +0800

    fix: mysql dependency

--- view-plugins/rabbit-view/src/main/java/org/tview/visualization/common/cache/TestLog.java
+++ view-plugins/rabbit-view/src/main/java/org/tview/visualization/common/cache/TestLog.java
@@ -1,5 +1,13 @@
 package org.tview.visualization.common.cache;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class TestLog {
+  private static final Logger log = LoggerFactory.getLogger(TestLog.class);
+
+  public static void main(String[] args) {
+    log.info("rabbit ");
+  }
 
 }
