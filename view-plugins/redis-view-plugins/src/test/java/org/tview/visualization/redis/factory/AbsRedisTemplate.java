7b54d00b2313cd7987715789e51e4813ec9c3c78
huifer <huifer97@163.com>
2020-07-20 19:48:51 +0800

    fix: redis dependency

--- view-plugins/redis-view-plugins/src/test/java/org/tview/visualization/redis/factory/AbsRedisTemplate.java
+++ view-plugins/redis-view-plugins/src/test/java/org/tview/visualization/redis/factory/AbsRedisTemplate.java
@@ -1,11 +1,14 @@
 package org.tview.visualization.redis.factory;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.tview.visualization.model.redis.RedisConnectionConfig;
 
 public class AbsRedisTemplate {
+
   public RedisConnectionConfig config;
 
+  @DisplayName(value = "init_redis_config")
   @BeforeEach
   public void init() {
     config = new RedisConnectionConfig();
