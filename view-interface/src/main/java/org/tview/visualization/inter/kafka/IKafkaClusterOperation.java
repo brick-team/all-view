20a9e88db3059e207a6cc8d28e9390e9a990bba4
huifer <huifer97@163.com>
2020-07-22 09:48:22 +0800

    feat: kafka cluster and message operation

--- view-interface/src/main/java/org/tview/visualization/inter/kafka/IKafkaClusterOperation.java
+++ view-interface/src/main/java/org/tview/visualization/inter/kafka/IKafkaClusterOperation.java
@@ -1,3 +1,15 @@
 package org.tview.visualization.inter.kafka;
 
-public interface IKafkaClusterOperation {}
+import org.tview.visualization.model.kafka.ClusterDescription;
+import org.tview.visualization.model.kafka.KafkaConnectionConfig;
+
+/**
+ * 集群操作
+ */
+public interface IKafkaClusterOperation {
+    /**
+     * kafka 集群信息
+     * @return
+     */
+    ClusterDescription description(KafkaConnectionConfig kafkaConnectionConfig);
+}
---
+++ view-plugins/kafka-view-plugins/src/main/java/org/tview/visualization/impl/IKafkaClusterOperationImpl.java
@@ -0,0 +1,47 @@
+package org.tview.visualization.impl;
+
+import java.util.Collection;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.DescribeClusterResult;
+import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tview.visualization.factory.KafkaConnectionFactory;
+import org.tview.visualization.factory.KafkaConnectionFactoryImpl;
+import org.tview.visualization.inter.kafka.IKafkaClusterOperation;
+import org.tview.visualization.model.kafka.ClusterDescription;
+import org.tview.visualization.model.kafka.KafkaConnectionConfig;
+
+public class IKafkaClusterOperationImpl implements IKafkaClusterOperation {
+    private static final Logger LOG = LoggerFactory.getLogger(IKafkaClusterOperationImpl.class);
+    KafkaConnectionFactory factory = new KafkaConnectionFactoryImpl();
+    /**
+     * kafka 集群信息
+     *
+     * @return
+     */
+    @Override
+    public ClusterDescription description(KafkaConnectionConfig kafkaConnectionConfig) {
+        AdminClient adminClient = factory.factoryAdminClient(kafkaConnectionConfig);
+        DescribeClusterResult describeClusterResult = adminClient.describeCluster();
+        try {
+            Collection<Node> nodes = describeClusterResult.nodes().get();
+            KafkaFuture<Node> controller = describeClusterResult.controller();
+            String clusterId = describeClusterResult.clusterId().get();
+
+            ClusterDescription clusterDescription = new ClusterDescription();
+            clusterDescription.setClusterId(clusterId);
+            clusterDescription.setController(controller.get());
+            clusterDescription.setNodes(nodes);
+            return clusterDescription;
+        } catch (InterruptedException | ExecutionException e) {
+            LOG.error("获取卡夫卡集群失败,{}", e);
+
+        }
+        return null;
+
+    }
+}
